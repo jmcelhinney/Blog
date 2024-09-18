c9c4086ec26b725ad53402c47d897691f26b0a9c
IvanTheeTerror <102115336+IvanTheeTerror@users.noreply.github.com>
2024-09-18 20:58:59 +1000

    Initial commit

---
+++ components/PageTitle.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from 'react'
+
+interface Props {
+  children: ReactNode
+}
+
+export default function PageTitle({ children }: Props) {
+  return (
+    <h1 className="text-2xl font-extrabold leading-8 tracking-tight text-gray-900 dark:text-gray-100 sm:text-3xl sm:leading-9 md:text-4xl md:leading-10">
+      {children}
+    </h1>
+  )
+}
