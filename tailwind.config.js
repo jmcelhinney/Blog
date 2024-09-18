79cd866deacea60a9768a68387722bbe95be29fd
jmcelhinney <cargo_censer0p@icloud.com>
2024-09-18 22:22:19 +1000

    Updated configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,7 +25,7 @@ module.exports = {
         sans: ['var(--font-space-grotesk)', ...fontFamily.sans],
       },
       colors: {
-        primary: colors.indigo,
+        primary: colors.yellow,
         gray: colors.gray,
       },
       zIndex: {
